b1a654f0869a051d73455b745315d16e94ede86d
haiduc159205hsr <haiduc159205.hsr@outlook.com>
2025-07-24 20:38:00 +0700

    cicd new

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore; // ✅ PHẢI CÓ DÒNG NÀY
+
 namespace cicd
 {
     public class Program
@@ -8,6 +10,9 @@ namespace cicd
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.WebHost.UseUrls("http://*:80");
 
